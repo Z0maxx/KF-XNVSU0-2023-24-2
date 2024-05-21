3131c6b21a60488864f1905107db30d32a6decaf
nyari.zoltan@stud.uni-obuda.hu <nyari.zoltan@stud.uni-obuda.hu>
2024-05-21 16:46:27 +0200

    added auth routes

--- smart-start/src/router/index.ts
+++ smart-start/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "@/components/Home.vue";
+import Login from "@/components/Login.vue";
+import Logout from "@/components/Logout.vue";
+import Register from "@/components/Register.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -7,11 +10,26 @@ const routes: Array<RouteRecordRaw> = [
     name: "home",
     component: Home,
   },
-];
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/logout',
+    name: 'logout',
+    component: Logout
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-});
+})
 
 export default router;
