cb3469b392015b20b45c1c524bf20434c0f96d2c
nyari.zoltan@stud.uni-obuda.hu <nyari.zoltan@stud.uni-obuda.hu>
2024-05-24 20:21:09 +0200

    added model store validations

--- smart-start/src/stores/comment.ts
+++ smart-start/src/stores/comment.ts
@@ -1,12 +1,32 @@
-import { Comment } from '@/types'
+import { Comment, FetchError, NetworkError, Validations } from '@/types'
 import { defineStore } from 'pinia'
 import { crudStore } from './crud-store'
+import { ref } from 'vue'
 
 const model = 'Comment'
 
 export const useCommentStore = defineStore(model, () => {
+    const commentValidations = ref<Validations | undefined>(undefined)
     const crud = crudStore()
     const { submitItem, deleteItem } = crud
+
+    async function setCommentValidations() {
+        if (commentValidations.value !== undefined) return
+
+        submitComment({
+            id: '',
+            ideaId: '',
+            ownerId: '',
+            message: ''
+        }).catch((err: FetchError) => {
+            if ('errors' in err && err.status === 400) {
+                commentValidations.value = err.errors
+            }
+            else {
+                throw err
+            }
+        })
+    }
 
     async function submitComment(comment: Comment) {
         return await submitItem(model, comment)
@@ -17,6 +37,8 @@ export const useCommentStore = defineStore(model, () => {
     }
 
     return {
+        commentValidations,
+        setCommentValidations,
         submitComment,
         deleteComment
     }
