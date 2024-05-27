b2b6b974aae777fd95151a7c778a489a65916dee
Z0maxx <110729156+Z0maxx@users.noreply.github.com>
2024-05-27 22:58:10 +0200

    Create Logout.vue

---
+++ smart-start/src/components/Logout.vue
@@ -0,0 +1,17 @@
+<template>
+    Logout
+</template>
+
+<script lang="ts" setup>
+import router from '@/router';
+import { useAuthStore } from '@/stores/auth';
+import { onMounted } from 'vue';
+
+const store = useAuthStore()
+const { logout } = store
+
+onMounted(() => {
+    logout()
+    router.push({ path: '/' })
+})
+</script>
