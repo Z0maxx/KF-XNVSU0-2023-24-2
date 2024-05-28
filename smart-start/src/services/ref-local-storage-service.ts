94955c29b1b4067800667bb68656fb3b374f2dfc
Z0maxx <nyari.zoltan500@gmail.com>
2024-05-28 08:00:35 +0200

    set ref now saves the ref

--- smart-start/src/services/ref-local-storage-service.ts
+++ smart-start/src/services/ref-local-storage-service.ts
@@ -1,16 +1,19 @@
 import { Ref } from "vue"
 
+const refs: Record<string, Ref<unknown>> = { }
+
 export function setRef(key: string, ref: Ref<unknown>) {
+    refs[key] = ref
     const item = localStorage.getItem(key)
     ref.value = item ? JSON.parse(item) : undefined
 }
 
-export function setItem(key: string, value: unknown, ref: Ref<unknown>) {
+export function setItem(key: string, value: unknown) {
     localStorage.setItem(key, JSON.stringify(value))
-    ref.value = value
+    refs[key].value = value
 }
 
-export function removeItem(key: string, ref: Ref<unknown>) {
+export function removeItem(key: string) {
     localStorage.removeItem(key)
-    ref.value = undefined
+    refs[key].value = undefined
 }
